7c10abdcddda804ac18399b9cab351321d690da7
Saqib Saud <saqib.saud@techlogix.com>
2016-08-11 13:07:35 +0500

    Initial Commit

---
+++ HIPChatMessages/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HIPChatMessages
+//
+//  Created by Saqib Saud on 11/08/2016.
+//  Copyright © 2016 Saqib Saud. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
